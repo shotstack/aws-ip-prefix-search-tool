d72805b7b670daa68452dfffb03117a3f51174fc
Jeff Shillitto <jeff@shillitto.com>
2023-10-05 13:24:15 +1100

    Initial commit - add AWS IP search tool

---
+++ README.md
@@ -0,0 +1,10 @@
+# AWS IP Prefix Search Tool
+
+This tool is used to search for AWS IP prefixes by region or service, instead of by IP address.
+
+It is a simple web page with an interface to search the [AWS IP address ranges](https://docs.aws.amazon.com/vpc/latest/userguide/aws-ip-ranges.html).
+It downloads the [ip-ranges.json](https://ip-ranges.amazonaws.com/ip-ranges.json) file and makes it searchable.
+
+You can use the tool here: [https://shotstack.io/tools/aws-ip-search-tool/](https://shotstack.io/tools/aws-ip-search-tool/).
+
+The webpage and build script were built almost entirely using ChatGPT in about an hour.
---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Load the environment variables from the .env file
+if [ -f .env ]; then
+  source .env
+else
+  echo "Error: .env file not found"
+  exit 1
+fi
+
+# Check if the GA_ID variable is provided
+if [ -z "$GA_ID" ]; then
+  echo "Error: GA_ID not found in .env file"
+  exit 1
+fi
+
+# Define the paths
+HTML_FILE="index.html"
+BUILD_FOLDER="build"
+
+# Create the build folder if it doesn't exist
+mkdir -p "$BUILD_FOLDER"
+
+# Copy the original HTML file to the build folder
+cp "$HTML_FILE" "$BUILD_FOLDER"
+
+# Generate the new HTML file with the updated Google Analytics ID
+NEW_HTML_FILE="$BUILD_FOLDER/$HTML_FILE"
+sed "s/GA_ID/$GA_ID/g" "$HTML_FILE" > "$NEW_HTML_FILE"
+
+echo "Google Analytics ID updated to $GA_ID in $NEW_HTML_FILE"
